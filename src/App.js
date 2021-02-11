dbda431382cc05c0cd4d69ebb145a408d303fbd4
Arjun <arjuntarakesh@gmail.com>
2021-02-11 14:50:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React , {useState, useEffect } from "react"
+import axios from "axios"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Coin from "./Coin";
+import logo from "./bitinr.png"
+
+
 
 function App() {
+
+  const [coins,setCoins] = useState([]);
+  const [search,setSearch] = useState('');
+
+ 
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+      console.log(res.data);
+    })
+    // promises based async await
+    .catch(error => alert('Error hitting API'));
+  },[])
+
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="coin-app">
+      <img src={logo} className="logo" alt="logo"/>
+      <h1 className="head">INR CRYPTO TRACKER</h1>
+      
+      
+      <div className="coin-searc">
+        <h1 className="coin-h">Search for currency</h1>
+        <form>
+          <input type="text" placeholder="🔍Search " className="coin-input" onChange={handleChange} />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return(
+          <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} volume={coin.total_volume}
+          price={coin.current_price} 
+          marketCap={coin.market_cap}
+          marketCapRank={coin.market_cap_rank}
+          priceChange={coin.price_change_percentage_24h}
+          
+          circulateSupply={coin.circulating_supply}
+          totalSupply={coin.total_supply}
+          maxSupply={coin.max_supply}
+          
+          athvalue={coin.ath}
+          athPercentage={coin.ath_change_percentage}
+          athDate={coin.ath_date}
+          lastUpdate={coin.last_updated}
+          
+          />
+        )
+      })}
+      <div className="foot">
+        
+          <p>Developed by Arjun T</p>
+          <button className="git"><a href="https://github.com/arjun-tarakesh" target="_blank" >Github</a></button>
+          
+         </div>
     </div>
   );
 }
---
+++ src/Coin.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import"./Coin.css"
+import {Container, Row, Col} from "reactstrap"
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+const Coin = ({name, image, symbol, price, volume, priceChange,marketCap,marketCapRank, circulateSupply, totalSupply, maxSupply, athvalue, athPercentage, athDate, lastUpdate}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+    
+    return (
+        <Container className="coin-container">
+            <Row className="coin-row">
+                <Col xl="2" lg="2" md="12" sm="12"  className="coin-col1"><img src={image} alt="crypto" className="coin-img" />
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                    
+                </Col>
+
+
+                <Col xl="2" lg="2" md="12" sm="12"  className="coin-col">
+                <p className="coin-price">Value ₹{price.toLocaleString()}</p></Col>
+
+
+                <Col xl="2" lg="2" md="12" sm="12" className="coin-col"><p className="coin-volume">Volume ₹{volume.toLocaleString()}</p></Col>
+                    {/* toLocaleString for the commas in the price */}
+                
+                <Col xl="2" lg="2" md="12" sm="12" className="coin-col">    
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">Change : {priceChange.toFixed(2)}% ⬇️</p>
+                        //  to fixed for .2 decimal places
+                    ) : (
+                        <p className="coin-percent green">Change : {priceChange.toFixed(2)}% 
+                        ⬆️ </p>
+                        //  to fixed for .2 decimal places
+                    ) }
+
+                
+                
+                </Col>
+
+
+                <Col xl="2" lg="2" md="12" sm="12" className="coin-col" >
+                    <p className="coin-mktcap">MarketCap  ₹{marketCap.toLocaleString()}</p>
+                </Col>
+
+
+                <Col xl="2" lg="2" md="12" sm="12" className="coin-col"><p className="coin-mktcaprank">Rank :  #{marketCapRank.toLocaleString()}</p></Col>
+                <Col xl="12" lg="12" md="12" sm="12" className="stats-col">
+                <div>
+                        <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }} className="stats">Toggle</Button>
+                        <Collapse isOpen={isOpen}>
+                            <Card className="stats-details">
+                            <CardBody className="stats-body">
+                            
+                            <p>Circulating Supply : ₹{circulateSupply}</p>
+                            <p>Total Supply : ₹{totalSupply}</p>
+                            <p>Maximum Supply : ₹{maxSupply}</p>
+                            <p>ATH : ₹{athvalue}</p>
+                            <p>ATH % : {athPercentage}%</p>
+                            <p>ATH date : {athDate}</p>
+                            <p>Last Updated : {lastUpdate}</p>
+                            </CardBody>
+                            </Card>
+                        </Collapse>
+                    </div></Col>
+
+                
+            </Row>
+        </Container>
+        
+        
+            
+        
+    )
+}
+
+export default Coin
